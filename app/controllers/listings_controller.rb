dd21e906569185617cdd011df406c057df61f4aa
Christopher Baker <chris@Christophers-MacBook-Pro.local>
2021-05-24 14:09:37 +1000

    init commit

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,27 @@
+class ListingsController < ApplicationController
+
+    def index
+
+    end
+
+    def new
+
+    end
+
+    def update
+
+    end
+
+    def delete
+
+    end
+
+    def edit
+
+    end
+
+    def show
+
+    end
+
+end
